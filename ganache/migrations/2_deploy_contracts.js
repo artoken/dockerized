02b5d6d6a65c818805f4a9b460704155a3bcc0af
Maxim <enginer385@gmail.com>
2021-11-20 17:47:35 +1000

    Fixes

--- ganache/migrations/2_deploy_contracts.js
+++ ganache/migrations/2_deploy_contracts.js
@@ -1,9 +1,18 @@
-var ART_TOKEN = artifacts.require("./Diamond.sol");
+var Diamond = artifacts.require("./Diamond.sol");
 var AuctionBox = artifacts.require("./AuctionBox.sol");
+var AuctionBoxClosed = artifacts.require("./ClosedAuctionBox.sol");
+var Sorter = artifacts.require("./InsertionSorter.sol");
+var ERC20 = artifacts.require("./ArtERC20Token.sol");
 
-module.exports = async function(deployer) {
-  await deployer.deploy(ART_TOKEN);
+module.exports = async function(deployer, _, accounts) {
+  await deployer.deploy(Diamond);
   await deployer.deploy(AuctionBox);
-  //token = await Token.deployed();
-  //await token.passMinterRole(dBank.address);
+  var InsertSorter = await deployer.deploy(Sorter);
+  await deployer.link(Sorter, AuctionBoxClosed);
+  await deployer.deploy(AuctionBoxClosed)
+  var erc = await deployer.deploy(ERC20)
+  console.log(accounts)
+  for(var i =0; i<10; i++) {
+    await erc.mint(accounts[i], 1000000000000, {'from': accounts[0]})
+  }
 };
